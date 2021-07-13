1aac53b5e04fae02eab286dafc3d13696f009698
Gavin Suddrey <g.suddrey@qut.edu.au>
2021-07-13 16:03:26 +1000

    Fixing ROS backedn

--- armer/backends/ROS/ROS.py
+++ armer/backends/ROS/ROS.py
@@ -59,3 +59,78 @@ class ROS(Connector):  # pragma nocover
             self.robots[ob.name] = ob
 
         super().add()
+
+    def step(self):
+        '''
+        step(dt) triggers the external program to make a time step
+        of defined time updating the state of the environment as defined by
+        the robot's actions.
+
+        The will go through each robot in the list and make them act based on
+        their control type (position, velocity, acceleration, or torque). Upon
+        acting, the other three of the four control types will be updated in
+        the internal state of the robot object. The control type is defined
+        by the robot object, and not all robot objects support all control
+        types.
+
+        '''
+
+        pass
+
+    def reset(self):
+        '''
+        reset() triggers the external program to reset to the
+        original state defined by launch
+
+        '''
+
+        pass
+
+    def restart(self):
+        '''
+        restart() triggers the external program to close and relaunch
+        to thestate defined by launch
+
+        '''
+
+        pass
+
+    def close(self):
+        '''
+        close() triggers the external program to gracefully close
+
+        '''
+
+        pass
+
+    #
+    #  Methods to interface with the robots created in other environemnts
+    #
+
+    def add(self):
+        '''
+        id = add(object) adds the object to the external environment. object must
+        be of an appropriate class. This adds a object object to a list of
+        objects which will act upon the step() method being called.
+
+        '''
+
+        pass
+
+    def remove(self):
+        '''
+        remove(id) removes the object from the external environment.
+
+        '''
+
+        pass
+
+    def hold(self):    # pragma nocover
+        '''
+        hold() keeps the backend open i.e. stops the program from closing once
+        the main script has finished. This method may need keep an even loop
+        running for the backend to keep it responsive.
+
+        '''
+
+        pass
